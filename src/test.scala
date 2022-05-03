b3450933a575c2dfd72503235118deede45e2ac0
Richard Guan <rguan72@gmail.com>
2022-05-02 21:48:21 -0400

    Add new files for start of learning scala

--- src/test.scala
+++ src/test.scala
@@ -1,3 +1,11 @@
+import collection.mutable.Stack
+
 object Test:
   def main(args: Array[String]) =
     println("Hello, world!")
+    val x = new Stack[Int]
+    val y = methodBro[Int]
+    println(y)
+
+  def methodBro[A] =
+    Nil
